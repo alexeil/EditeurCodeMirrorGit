96870846a4ac0f70545286f80c1a8b3259fb416a
toto test <alexeil@hotmail.fr>
2012-06-07 17:03:01 +0200

    Commit pop ups !

--- kevoree-library/javase/org.kevoree.library.javase.webserver.collaborationToolsBasics/src/main/java/org/kevoree/library/javase/webserver/collaborationToolsBasics/client/IHMcodeMirror.java
+++ kevoree-library/javase/org.kevoree.library.javase.webserver.collaborationToolsBasics/src/main/java/org/kevoree/library/javase/webserver/collaborationToolsBasics/client/IHMcodeMirror.java
@@ -1,96 +1,144 @@
 package org.kevoree.library.javase.webserver.collaborationToolsBasics.client;
 
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.RepeatingCommand;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Event.NativePreviewEvent;
+import com.google.gwt.user.client.Event.NativePreviewHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
-
-
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class IHMcodeMirror implements EntryPoint {
-
-    private TextArea textAreaInsert, textAreaShow;
-    private TextBox textBoxLogin, textBoxPassword, textBoxNameRepository;
-    private Button btnPush;
+    private TextBox textBoxLogin, textBoxPassword, textBoxNameRepository,
+            textBoxURLRepository, textBoxLogin2, textBoxPassword2;
     private String login, password, nomRepository;
-    private PopupPanel popupForm;
+    private PopupPanel popupFormNew, popupFormOpen;
+    private Button btnOpen, btnSave;
+    private NativeEvent ne;
 
-    private final RepositoryToolsServicesAsync repositoryToolsServices = GWT
-            .create(RepositoryToolsServices.class);
+
+      private final RepositoryToolsServicesAsync repositoryToolsServices = GWT
+       .create(RepositoryToolsServices.class);
+
 
     /**
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        // Add the nameField and sendButton to the RootPanel
-        // Use RootPanel.get() to get the entire body element
+
         RootPanel rootPanel = RootPanel.get();
 
+        // general Layout
         Grid grid = new Grid(2, 2);
-        rootPanel.add(grid, 0, 0);
-        grid.setSize("600px", "600px");
+        rootPanel.add(grid, 10, 0);
+        grid.setSize("", "");
 
-        popupForm = new PopupPanel(true);
+        // Form open existing project
+        popupFormOpen = new PopupPanel(true);
+        Grid gridFieldsOpen = new Grid(4, 2);
+        Label lblURLRepository = new Label("HTTPS du repository :");
 
-        Grid gridFields = new Grid(4,2);
+        Button btnNouveau = new Button("New");
+        btnNouveau.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                popupFormNew.show();
+            }
+        });
 
+        grid.setWidget(0, 0, btnNouveau);
+        btnNouveau.setWidth("123px");
+
+        btnSave = new Button("Save");
+        btnSave.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent clickEvent) {
+
+                      repositoryToolsServices.pushRepository(login, password,new
+                      AsyncCallback<Boolean>(){
+
+                      @Override public void onFailure(Throwable throwable) { }
+
+                      @Override public void onSuccess(Boolean aBoolean) {  }
+
+                      });
+
+            }
+        });
+
+        btnOpen = new Button("Open");
+        grid.setWidget(0, 1, btnOpen);
+        btnOpen.setWidth("125px");
+
+        btnOpen.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                popupFormOpen.show();
+            }
+        });
+
+        Button btnImport = new Button("Import");
+        btnImport.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                popupFormOpen.hide();
+                btnSave.setEnabled(true);
+            }
+        });
+
+        grid.setWidget(1, 0, btnSave);
+        btnSave.setWidth("125px");
+        btnSave.setEnabled(false);
+
+        popupFormOpen.setWidget(gridFieldsOpen);
+
+        // Form New project
+        popupFormNew = new PopupPanel(true);
+        Grid gridFieldsNew = new Grid(4, 2);
+
+        // component for form new
         Label lblLogin = new Label("Login :");
-
-        gridFields.setWidget(0, 0, lblLogin);
+        gridFieldsNew.setWidget(0, 0, lblLogin);
         lblLogin.setHeight("25px");
 
         textBoxLogin = new TextBox();
-        gridFields.setWidget(0, 1, textBoxLogin);
+        gridFieldsNew.setWidget(0, 1, textBoxLogin);
 
         Label lblPassword = new Label("Password :");
-        gridFields.setWidget(1, 0, lblPassword);
+        gridFieldsNew.setWidget(1, 0, lblPassword);
 
         textBoxPassword = new TextBox();
-        gridFields.setWidget(1, 1, textBoxPassword);
+        gridFieldsNew.setWidget(1, 1, textBoxPassword);
 
         Label lblNomDuRepository = new Label("Nom du repository :");
-        gridFields.setWidget(2, 0, lblNomDuRepository);
+        gridFieldsNew.setWidget(2, 0, lblNomDuRepository);
 
         textBoxNameRepository = new TextBox();
-        gridFields.setWidget(2, 1, textBoxNameRepository);
+        gridFieldsNew.setWidget(2, 1, textBoxNameRepository);
 
-        btnPush = new Button("push");
-        btnPush.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent clickEvent) {
-                repositoryToolsServices.pushRepository(login, password,new AsyncCallback<Boolean>(){
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        //To change body of implemented methods use File | Settings | File Templates.
-                    }
+        // component for form Open
+        Label lblLogin2 = new Label("Login :");
+        lblLogin2.setHeight("25px");
+        textBoxLogin2 = new TextBox();
+        Label lblPassword2 = new Label("Password :");
+        textBoxPassword2 = new TextBox();
 
-                    @Override
-                    public void onSuccess(Boolean aBoolean) {
-                        //To change body of implemented methods use File | Settings | File Templates.
-                    }
-
-                });
-            }
-        });
-
-
-
-        Button btnNouveau = new Button("Nouveau");
-        btnNouveau.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                popupForm.show();
-            }
-        });
-
-
-
+        textBoxURLRepository = new TextBox();
+        gridFieldsOpen.setWidget(0, 0, lblLogin2);
+        gridFieldsOpen.setWidget(0, 1, textBoxLogin2);
+        gridFieldsOpen.setWidget(1, 0, lblPassword2);
+        gridFieldsOpen.setWidget(1, 1, textBoxPassword2);
+        gridFieldsOpen.setWidget(2, 0, lblURLRepository);
+        gridFieldsOpen.setWidget(2, 1, textBoxURLRepository);
+        gridFieldsOpen.setWidget(3, 0, btnImport);
 
         Button btnCreateRepo = new Button("Create repository");
         btnCreateRepo.addClickHandler(new ClickHandler() {
@@ -100,66 +148,94 @@ public class IHMcodeMirror implements EntryPoint {
                 password = textBoxPassword.getText();
                 nomRepository = textBoxNameRepository.getText();
 
-                repositoryToolsServices.createRepository(login,password,nomRepository, new AsyncCallback<Boolean>(){
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        //To change body of implemented methods use File | Settings | File Templates.
-                    }
 
-                    @Override
-                    public void onSuccess(Boolean aBoolean) {
-                        btnPush.setEnabled(true);
-                        textAreaInsert.setEnabled(true);
-                        popupForm.hide();
+                      repositoryToolsServices.createRepository(login,password,
+                      nomRepository, new AsyncCallback<Boolean>(){
 
-                    }
+                      @Override public void onFailure(Throwable throwable) {  }
 
-                });
-                btnPush.setEnabled(true);
-                textAreaInsert.setEnabled(true);
-                popupForm.hide();
+                      @Override public void onSuccess(Boolean aBoolean) {
+                          btnSave.setEnabled(true) ;
+                          popupFormNew.hide();
+
+                      }
+
+                      });
             }
         });
 
-        gridFields.setWidget(3, 0, btnCreateRepo);
+        gridFieldsNew.setWidget(3, 0, btnCreateRepo);
 
-        grid.setWidget(0, 0, btnNouveau);
-        grid.setWidget(0, 1, btnPush);
-        btnPush.setWidth("125px");
-        btnPush.setEnabled(false);
+        popupFormNew.setWidget(gridFieldsNew);
+        popupFormOpen.setWidget(gridFieldsOpen);
 
-        textAreaShow = new TextArea();
-        textAreaShow.setEnabled(false);
-        grid.setWidget(1, 1, textAreaShow);
-        textAreaShow.setSize("203px", "176px");
+        HandlerRegistration logHandler = Event
+                .addNativePreviewHandler(new NativePreviewHandler() {
 
-        textAreaInsert = new TextArea();
-        textAreaInsert.addKeyUpHandler(new KeyUpHandler() {
+                    @Override
+                    public void onPreviewNativeEvent(NativePreviewEvent event) {
+                        ne = event.getNativeEvent();
+
+                        if (ne.getType().equals("keydown")
+                                && ne.getCtrlKey()
+                                && (ne.getKeyCode() == 'n' || ne.getKeyCode() == 'N')) {
+                            ne.preventDefault();
+                            Scheduler.get().scheduleDeferred(
+                                    new ScheduledCommand() {
+                                        @Override
+                                        public void execute() {
+                                            if (popupFormNew.isShowing()) {
+                                                popupFormNew.hide();
+                                            } else {
+                                                popupFormNew.show();
+                                            }
+                                        }
+                                    });
+                        } else if (ne.getType().equals("keydown")
+                                && ne.getCtrlKey()
+                                && (ne.getKeyCode() == 'o' || ne.getKeyCode() == 'O')) {
+                            ne.preventDefault();
+                            Scheduler.get().scheduleDeferred(
+                                    new ScheduledCommand() {
+                                        @Override
+                                        public void execute() {
+                                            if (popupFormOpen.isShowing()) {
+                                                popupFormOpen.hide();
+                                            } else {
+                                                popupFormOpen.show();
+                                            }
+                                        }
+                                    });
+                        } else if (ne.getType().equals("keydown")
+                                && ne.getCtrlKey()
+                                && (ne.getKeyCode() == 's' || ne.getKeyCode() == 'S')) {
+                            ne.preventDefault();
+
+                            Scheduler.get().scheduleDeferred(
+                                    new ScheduledCommand() {
+                                        @Override
+                                        public void execute() {
+                                            if (btnSave.isEnabled()) {
+                                                // fonction to call for saving
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+
+        // Auto-Save
+        int savePeriod = 5000;
+        Scheduler.get().scheduleFixedDelay(new RepeatingCommand() {
             @Override
-            public void onKeyUp(KeyUpEvent event) {
-                repositoryToolsServices.updateContentFileAndCommit(textAreaInsert.getText().getBytes(), login, new AsyncCallback<Boolean>() {
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        //To change body of implemented methods use File | Settings | File Templates.
-                    }
-
-                    @Override
-                    public void onSuccess(Boolean aBoolean) {
-                        textAreaShow.setText(textAreaInsert.getText());
-                    }
-
-                });
-                textAreaShow.setText(textAreaInsert.getText());
-
+            public boolean execute() {
+                if (btnSave.isEnabled()) {
+                    // fonction to call for saving
+                    System.err.println("Test --> " + System.currentTimeMillis());
+                }
+                return true;
             }
-        });
 
-        grid.setWidget(1, 0, textAreaInsert);
-        textAreaInsert.setSize("203px", "176px");
-        textAreaInsert.setEnabled(false);
-
-        popupForm.setWidget(gridFields);
-
-
+        }, savePeriod);
     }
 }
